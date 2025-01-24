6b6741dc19c6f2183bcc10fbedb13fa95cb73350
SilviaFranzinelli <franzinellisilvia@gmail.com>
2025-01-24 15:42:49 +0100

    1

---
+++ script1.js
@@ -0,0 +1,6 @@
+fetch("https://striveschool-api.herokuapp.com/api/esercizio/", {
+  headers: {
+    Authorization:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzYTIzN2YyMDE4YzAwMTU5MDc1NDIiLCJpYXQiOjE3Mzc3Mjg1NjcsImV4cCI6MTczODkzODE2N30.HbKyBUszLlxHruj0Uw9FYZNIWr26mftJtbo0cWAGep0"
+  }
+});

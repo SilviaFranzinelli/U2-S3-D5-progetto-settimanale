973e58689df01c0fd42b4ec05d96285600fcfb2d
SilviaFranzinelli <franzinellisilvia@gmail.com>
2025-01-26 19:48:50 +0100

    3

---
+++ backoffice.js
@@ -0,0 +1,25 @@
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzYTIzN2YyMDE4YzAwMTU5MDc1NDIiLCJpYXQiOjE3Mzc3Mjg1NjcsImV4cCI6MTczODkzODE2N30.HbKyBUszLlxHruj0Uw9FYZNIWr26mftJtbo0cWAGep0"
+  }
+});
+
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
+
+const form = document.getElementById("productForm");
+form.onsubmit = function (event) {
+  event.preventDefault();
+
+  const newProduct = {
+    name: form.elements.name.value,
+    description: form.elements.description.value,
+    brand: form.elements.brand.value,
+    imageUrl: form.elements.imageUrl.value,
+    price: form.elements.price.value
+  };
+
+  console.log(newProduct);
+
+  fetch(URL, { method: "POST", body: JSON.stringify(newProduct), headers: { "Conte-Type": "application/json" } });
+};
--- home.js
+++ home.js
@@ -7,8 +7,47 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
   .then((resp) => {
     if (resp.ok) {
       return resp.json();
+    } else {
+      throw new Error("Ci dispiace non siamo riusciti a reperire il dato");
     }
   })
   .then((product) => {
     console.log(product);
+
+    const row = document.getElementById("product-list");
+
+    product.forEach((prod) => {
+      console.log(prod);
+
+      const li = document.createElement("li");
+      li.classList.add("list-group-item", "d-flex", "justify-content-between", "alng-items-center");
+      li.innerHTML = `<span>${prod.name}</span> <span class="badge text-bg-primary">${prod.price}€ </span>`;
+
+      row.appendChild(li);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    generateAlert(err.message);
+  })
+
+  .finally(() => {
+    isLoading(false);
   });
+
+const isLoading = function (loadingState) {
+  const spinner = document.querySelector(".spinner-border");
+  if (loadingState) {
+    spinner.classList.remove("d-none");
+  } else {
+    spinner.classList.add("d-none");
+  }
+};
+
+const generateAlert = function (message) {
+  const alertContainer = document.getElementById("alert-container");
+
+  alertContainer.innerHTML = `<div class="alert alert-danger" role="alert">
+                                  ${message}
+                                </div>`;
+};
